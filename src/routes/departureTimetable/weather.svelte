bdc2ab633a3c95ff7064168ece9c76c0a53f3b57
Nada Stanisich <nstanisich@gmail.com>
2024-02-06 13:02:22 +1100

    renamed Weather.svelte

--- src/routes/departureTimetable/weather.svelte
+++ src/routes/departureTimetable/weather.svelte
@@ -12,12 +12,14 @@
 </script>
 
 <div class="text-lg font-bold text-gray-700 dark:text-white border border-gray-300 p-4 mt-4">
-	{#if weatherData.main}
-		<h1>{weatherData.name}</h1>
-		<div class="flex items-center">
-			<img src="http://openweathermap.org/img/w/{(weatherData.weather && weatherData.weather[0].icon) || ''}.png" alt="Weather Icon" />
-			<h5 class="mb-2 text-3xl font-bold tracking-tight text-gray-900 dark:text-white">{Math.round(weatherData.main.temp - 273.15)}°C</h5>
-		</div>
-		<p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">{weatherData.weather[0].description}</p>
+	{#if weatherData}
+		{#if weatherData.main}
+			<h1>{weatherData.name}</h1>
+			<div class="flex items-center">
+				<img src="http://openweathermap.org/img/w/{(weatherData.weather && weatherData.weather[0].icon) || ''}.png" alt="Weather Icon" />
+				<h5 class="mb-2 text-3xl font-bold tracking-tight text-gray-900 dark:text-white">{Math.round(weatherData.main.temp - 273.15)}°C</h5>
+			</div>
+			<p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">{weatherData.weather[0].description}</p>
+		{/if}
 	{/if}
 </div>
