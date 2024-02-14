f2c8dafe443e7fcf3f983304832bf797e38f2f68
Nada Stanisich <nstanisich@gmail.com>
2024-02-14 15:17:27 +1100

    dev differenciation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,13 @@
 <script>
 	import '../app.pcss';
+
+	const dev = import.meta.env.VITE_APP_ENV === 'development';
+	console.log('dev', dev);
+
 </script>
-<div class="h-screen bg-blue-400">	
-	<slot />
-</div>
+
+<main style={dev ? 'border-top: 9px solid red;' : ``}>
+	<div class="h-screen bg-blue-400">	
+		<slot />
+	</div>
+</main>
